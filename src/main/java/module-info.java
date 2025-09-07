8c8b3fa2566b4b0bd028a8d09898977fd5ce6bfc
AhmedQw1 <ahmedsalmen@gmail.com>
2025-09-07 16:50:50 +0400

    Update pom.xml and module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,27 +1,13 @@
 module com.example.soundplayerv1 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires javafx.media;
     requires java.prefs;
 
-    // Open the main package to FXML
     opens com.example.soundplayerv1 to javafx.fxml;
     exports com.example.soundplayerv1;
-
-    // Open the Controller package to FXML
     opens Controller to javafx.fxml;
     exports Controller;
-
-    // Export model and util packages if needed by other modules
     exports model;
     exports util;
 }
