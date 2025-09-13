6aafce9f2632b183169e211397c8b5f9189251f9
AhmedQw1 <ahmedsalmen@gmail.com>
2025-09-14 03:22:54 +0400

    Fixed jpackage configuration and added custom icon support

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,17 +2,23 @@ module com.example.soundplayerv1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    requires javafx.graphics;
+    requires javafx.base;
     requires java.prefs;
+    requires java.desktop;
 
-    // New modules
-    requires com.jfoenix;                     // JFoenix (automatic module name)
-    requires org.kordamp.ikonli.javafx;       // Ikonli JavaFX binding
-    requires org.kordamp.ikonli.fontawesome5; // Ikonli FontAwesome5 pack
-    requires org.kordamp.ikonli.materialdesign2; // Ikonli MaterialDesign2 pack
+    // JFoenix and Ikonli modules
+    requires com.jfoenix;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.materialdesign2;
 
+    // Open packages for reflection
     opens com.example.soundplayerv1 to javafx.fxml;
-    exports com.example.soundplayerv1;
     opens Controller to javafx.fxml;
+
+    // Export packages
+    exports com.example.soundplayerv1;
     exports Controller;
     exports model;
     exports util;
